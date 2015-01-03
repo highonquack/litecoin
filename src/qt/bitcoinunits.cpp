b8d2673164f06e914a676aaa809b687dbd88c66f
John Bridge <johnbridge180@gmail.com>
2015-01-03 16:36:12 -0500

    Changed from litecoin to steamcoin settings
    Swapped out litecoin settings/name/labels for steamcoin’s.

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("STM");
+    case mBTC: return QString("mSTM");
+    case uBTC: return QString::fromUtf8("μSTM");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Steamcoins");
+    case mBTC: return QString("Milli-Steamcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Steamcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
